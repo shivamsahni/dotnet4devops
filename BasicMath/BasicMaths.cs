539f9e10b579012fffb97455880c5aa9707df1d5
shivamsahni <shivamsahniss21@gmail.com>
2021-07-13 23:40:41 +0530

    sample code with UT initial commit

---
+++ BasicMath/BasicMaths.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicMath
+{
+
+    public class BasicMaths
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            // to trace error while testing, writing + opertaor instead of * operator
+            return num1 + num2;
+        }
+    }
+
+}
---
+++ BasicMathTests/BasicMathsTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicMath;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicMath.Tests
+{
+    [TestClass()]
+    public class BasicMathsTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.Add(2, 3);
+            Assert.AreEqual(result, 5, "Add method gave wrong answer");
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.Subtract(3, 2);
+            Assert.AreEqual(result, 1, "Subtract method gave wrong answer");
+        }
+
+        [TestMethod()]
+        public void divideTest()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.divide(4, 2);
+            Assert.AreEqual(result, 2, "Divide method gave wrong answer");
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.Multiply(2, 3);
+            Assert.AreEqual(result, 6, "Multiply method gave wrong answer");
+        }
+    }
+}
